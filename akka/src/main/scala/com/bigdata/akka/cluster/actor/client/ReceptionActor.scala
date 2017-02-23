1a26077c5b7d9ec7ced4c2a12605a60c39f59e42
kkang2 <PSJ@PSJ-PC>
2017-02-23 15:36:32 +0900

    akka cluster client 관련 커밋

---
+++ akka/src/main/scala/com/bigdata/akka/cluster/actor/client/ReceptionActor.scala
@@ -0,0 +1,15 @@
+package com.bigdata.akka.cluster.actor.client
+
+import akka.actor.ActorLogging
+import akka.actor.Actor
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator.Publish
+
+class ReceptionActor extends Actor with ActorLogging {
+	val mediator = DistributedPubSub(context.system).mediator
+	
+	def receive = {
+		case msg:String 	=> log.debug("ReceptionActor receive msg : {}, self : {}", msg, self)
+    	case _ @ etc 			=> log.debug("etc msg : {}, self : {}", etc, self)
+  	}
+}
