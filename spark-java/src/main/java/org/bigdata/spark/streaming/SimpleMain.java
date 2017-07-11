dbe5f7bcb244e1c07240996ffc1a9ae3a9de7fe2
mallakkangyi <mallakkangyi@mobigen.com>
2017-07-11 17:03:11 +0900

    테스트 코드 커밋

---
+++ spark-java/src/main/java/org/bigdata/spark/streaming/SimpleMain.java
@@ -0,0 +1,21 @@
+package org.bigdata.spark.streaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SimpleMain {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("SimpleApp");
+		
+		/* SparkConf 의 set 메소드로 컨피그 설정가능함.
+		conf.set("spark.app.name", "SimpleApp");
+		conf.set("spark.master", "local");*/
+		
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		JavaRDD<String> contents = sc.textFile("src/main/resources/text1.txt");
+		
+		System.out.println(contents.count());
+	}
+}
